9abe6c40a0d3a7f325e7c5a23d15282862dd50d7
chenjunqi <chenjunqi@thunisoft.com>
2022-03-31 15:27:06 +0800

    add test string intern

---
+++ src/com/cjq/outofmemory/intern/TestIntern.java
@@ -0,0 +1,36 @@
+/*
+ * @projectName CjqOutOfMemory
+ * @package com.cjq.outofmemory.intern
+ * @className com.cjq.outofmemory.intern.TestIntern
+ * @copyright Copyright 2022 Thuisoft, Inc. All rights reserved.
+ */
+package com.cjq.outofmemory.intern;
+
+/**
+ * TestIntern
+ * @description java8测试String的intern方法，和java6比较
+ * @author chenjunqi
+ * @date 2022年03月30日 下午8:39
+ * @version 3.0.0
+ */
+public class TestIntern {
+
+    /**
+     * TestIntern
+     * @description 返回值为false、true，
+     * java8的字符串常量池移到了堆中，所以只需要记录常量第一次出现的引用
+     * 计算机科学是第一次进入常量池，所以true
+     * java这个常量在sun.mics.Version加载时进入常量池，所以s2进入常量池操作时候，返回的第一次进入常量池的引用，所以为false
+     * @param args args
+     * @author chenjunqi
+     * @date 2022/3/30 下午8:42
+     * @version 3.0.0
+     */
+    public static void main(String[] args) {
+        String s1 = new StringBuilder("计算机").append("科学").toString();
+        System.out.println(s1 == s1.intern());
+
+        String s2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(s2 == s2.intern());
+    }
+}
---
+++ src/com/cjq/outofmemory/methodarea/CjqMethodAreaOOM.java
@@ -0,0 +1,42 @@
+/*
+ * @projectName CjqOutOfMemory
+ * @package com.cjq.outofmemory.methodarea
+ * @className com.cjq.outofmemory.methodarea.CjqMethodAreaOOM
+ * @copyright Copyright 2022 Thuisoft, Inc. All rights reserved.
+ */
+package com.cjq.outofmemory.methodarea;
+
+
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * CjqMethodAreaOOM
+ * @description 方法区oom，java8已经移出永久代，所以不会出现OutOfMemory Permgen Space
+ * @author chenjunqi
+ * @date 2022年03月30日 下午9:14
+ * @version 3.0.0
+ */
+public class CjqMethodAreaOOM {
+
+    public static void main(String[] args) {
+        while(true){
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(CjqCool.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                    return methodProxy.invoke(o,objects);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class CjqCool{}
+}
