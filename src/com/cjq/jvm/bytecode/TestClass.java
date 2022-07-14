dabbe3f6e531477d46e716114a844c44ea8ac3ce
chenjunqi <chenjunqi@thunisoft.com>
2022-07-14 21:12:15 +0800

    add bytecode has exception

---
+++ src/com/cjq/jvm/bytecode/TestClass.java
@@ -0,0 +1,34 @@
+/*
+ * @projectName CjqOutOfMemory
+ * @package com.cjq.jvm.bytecode
+ * @className com.cjq.jvm.bytecode.TestClass
+ * @copyright Copyright 2022 Thuisoft, Inc. All rights reserved.
+ */
+package com.cjq.jvm.bytecode;
+
+/**
+ * TestClass
+ * @description 测试字节码，有异常表
+ * @author chenjunqi
+ * @date 2022年07月14日 下午9:05
+ * @version 3.0.0
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println(new TestClass().inc());
+    }
+
+    public int inc() {
+        int x;
+        try {
+            x = 1;
+            return x;
+        } catch (Exception e) {
+            x = 2;
+            return x;
+        } finally {
+            x = 3;
+        }
+    }
+}
