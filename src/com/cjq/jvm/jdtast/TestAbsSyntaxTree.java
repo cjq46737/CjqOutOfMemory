0eeb26cb2f99d75713b49eb1aaec3dc83291f195
chenjunqi <chenjunqi@thunisoft.com>
2023-07-30 10:55:45 +0800

    测试抽象语法树插件

---
+++ src/com/cjq/jvm/jdtast/TestAbsSyntaxTree.java
@@ -0,0 +1,22 @@
+/*
+ * @projectName CjqOutOfMemory
+ * @package com.cjq.jvm.jdtast
+ * @className com.cjq.jvm.jdtast.TestAbsSyntaxTree
+ * @copyright Copyright 2023 Thuisoft, Inc. All rights reserved.
+ */
+package com.cjq.jvm.jdtast;
+
+/**
+ * TestAbsSyntaxTree
+ * @description 测试抽象语法树
+ * @author chenjunqi
+ * @date 2023年07月30日 上午10:43
+ * @version 3.0.0
+ */
+public class TestAbsSyntaxTree {
+
+    public void f() {
+        int result = 4 + 2 * 3 - 1;
+        System.out.println(result);
+    }
+}
